d6da694dad6f93842e2fc8de5e5848b0f6bbc408
Adrien Zaganelli <adrienzaganelli@gmail.com>
2024-01-05 21:22:38 +0100

    reset password for profile

--- packages/app/pages/profile.vue
+++ packages/app/pages/profile.vue
@@ -1,9 +1,19 @@
 <template>
   <main>
-    <form novalidate>
+    <form novalidate v-if="profile">
       <div>
-        <label>User id: </label>
-        <input readonly :value="user?.externalId" />
+        <label>Username</label>
+        <input readonly :value="profile.username" />
+      </div>
+      <div>
+        <label>Email</label>
+        <input readonly :value="profile.email" />
+      </div>
+
+      <div>
+        <button type="button" :disabled="status === 'pending' || status === 'success'" @click="resetPassword()">reset password </button>
+        <p v-if="status === 'success'">Your password reset link was sent to your inbox</p>
+        <p v-if="status === 'error'">{{error}}</p>
       </div>
     </form>
   </main>
@@ -15,4 +25,14 @@ definePageMeta({
 });
 
 const user = useUser();
+const { data: profile } = await useAsyncData(`${user.value?.externalId}-profile`, () => $fetch("/api/me"));
+
+
+const { status, refresh: resetPassword, error } = useLazyFetch('/api/reset-password', { 
+  immediate: false,
+  method: "POST",
+  body: {
+    email: profile.value?.email
+  }
+});
 </script>

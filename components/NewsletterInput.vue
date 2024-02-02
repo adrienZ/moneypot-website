4b55672c07133248a49e223d8c0eb7d5ed77a721
Adrien Zaganelli <adrienzaganelli@gmail.com>
2024-02-02 13:57:43 +0100

    fix margin

--- components/NewsletterInput.vue
+++ components/NewsletterInput.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="container">
+    <div class="container mx-auto">
       <div class="mb-12 text-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
